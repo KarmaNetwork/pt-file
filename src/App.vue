acc83d78bedd83fc25b5a8d1cfddf86e2b639885
tiannian <dtiannian@aliyun.com>
2019-07-26 15:15:00 +0800

    basic init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <b-form-file
+            v-model="file"
+            :state="Boolean(file)"
+            placeholder="Choose a file..."
+            drop-placeholder="Drop file here..."
+            ></b-form-file>
+        <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    components: {},
+    data() {
+        return {
+            file:null
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
