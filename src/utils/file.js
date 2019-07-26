d49590dd9a4bd12e1aefddc5a5a07837aefcf665
tiannian <dtiannian@aliyun.com>
2019-07-27 01:52:18 +0800

    add bootstrap

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,58 +1,5 @@
 import P2P from './connection.js'
 
-/*
-
-let state = {}
-
-state.promises = {};
-
-state.promises.recv_data = {};
-
-state.promises.recv_data.promise = new Promise( ( resolve, reject ) => {
-  state.promises.recv_data.resolve = resolve;
-  state.promises.recv_data.reject = reject;
-} );
-
-state.promises.on_open.promise = new Promise( ( resolve, reject ) => {
-  state.promises.on_open.resolve = resolve;
-  state.promises.on_open.reject = reject;
-} );
-
-state.ws = new WebSocket('ws://localhost:3000/bootstrap');
-
-state.ws.onopen(() => {
-  state.p2p = new P2P({
-    handlers: {
-      send_bootstrap(pk, data) {
-    let s = {
-      from: state.p2p.get_hex_key().publicKey,
-      to: pk,
-      data:data,
-    }
-    state.ws.send(JSON.stringify(s));
-      },
-      recv_data(pk, data) {
-    state.promises.recv_data.resolve({
-      pk: pk,
-      data: data,
-    })
-      }
-    }
-  });
-  console.log(`public key is ${state.p2p.get_hex_key().publicKey}`);
-  state.promises.on_open.resolve();
-});
-
-state.ws.onmessage = (data) => {
-  let m = JSON.parse(data);
-  if (m.op == 'signal') {
-    console.log(`recv signal from ${m.from}`);
-    state.p2p.recv_bootstrap(m.from, m.data );
-  }
-}
-
-*/
-
 export default class FileTransfer {
     constructor( prefix, sender ) {
         this.prefix = prefix;
@@ -131,6 +78,10 @@ export default class FileTransfer {
     getUrl(files) {
         this.files = files;
         return this.prefix + '/' + this.p2p.get_hex_key().publicKey;
+    }
+
+    bootstrap(pk) {
+        return this.p2p.bootstrap({pk:pk});
     }
 }
 
