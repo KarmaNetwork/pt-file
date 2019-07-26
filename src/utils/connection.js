5548fe3236b7ddc7d2d763d383c79afd5a2bfe22
clearloop <udtrokia@gmail.com>
2019-07-26 23:53:41 +0800

    fix yarn lint

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -4,159 +4,158 @@ const store = require('store');
 const Buffer = require('buffer').Buffer;
 
 class AlptP2P {
-    constructor ( opts ) {
-        // inital key
-        this.boxkeys = store.get('boxkeys');
-        if (this.boxkeys == undefined) {
-            this.boxkeys = nacl.box.keyPair();
-            store.set('boxkeys', this.boxkeys);
-        }
-
-        // Connections
-        this.peers = {};
-        if (opts.wrtc == undefined) {
-            this.env = 'browser';
-        } else {
-            this.env = 'wrtc';
-            this.wrtc = opts.wrtc;
-        }
-        // handler to send http server
-        this.handlers = opts.handlers;
+  constructor ( opts ) {
+    // inital key
+    this.boxkeys = store.get('boxkeys');
+    if (this.boxkeys === undefined) {
+      this.boxkeys = nacl.box.keyPair();
+      store.set('boxkeys', this.boxkeys);
     }
 
-    get_hex_key () {
-        let obj = {};
-        obj.publicKey = Buffer.from(this.boxkeys.publicKey).toString('hex');
-        obj.secretKey = Buffer.from(this.boxkeys.secretKey).toString('hex');
-        return obj;
+    // Connections
+    this.peers = {};
+    if (opts.wrtc === undefined) {
+      this.env = 'browser';
+    } else {
+      this.env = 'wrtc';
+      this.wrtc = opts.wrtc;
     }
+    // handler to send http server
+    this.handlers = opts.handlers;
+  }
 
-    async _boot_server(server, func) {
-        let ice = server.ice;
-        let pk = server.pk;
+  get_hex_key () {
+    let obj = {};
+    obj.publicKey = Buffer.from(this.boxkeys.publicKey).toString('hex');
+    obj.secretKey = Buffer.from(this.boxkeys.secretKey).toString('hex');
+    return obj;
+  }
 
-        let opts = { initiator:true };
-        if (this.env == 'wrtc') {
-            opts.wrtc = this.wrtc;
-        }
-        let peer = new Peer( opts );
+  async _boot_server(server, func) {
+    // let ice = server.ice;
+    let pk = server.pk;
 
-        // receive offer connection info.
-        peer.on('signal', async data => {
-            this.handlers.send_bootstrap(pk ,data);
-        });
-
-        peer.on('connect', () => {
-            this.peers[pk].connected = true;
-            console.log(pk, 'is connected')
-            func();
-        });
-
-        peer.on('close', () => {
-            // delete?
-            console.log(pk, 'is closed')
-            if (this.peers[pk]) {
-                this.peers[pk].connected = false;
-                this.peers[pk].peer.destroy();
-                delete this.peers[pk];
-            }
-        })
-
-        peer.on('error', (err) => {
-            // delete?
-            console.log(pk, 'is error')
-            delete this.peers[pk];
-            if (this.peers[pk]) {
-                this.peers[pk].connected = false;
-                this.peers[pk].peer.destroy();
-                delete this.peers[pk];
-            }
-        });
-
-        peer.on('data', data => {
-            // Need add crypto
-            this.handlers.recv_data(pk,data);
-        });
-
-        this.peers[pk] = {
-            peer: peer,
-            connected: false,
-            publicKey: Buffer.from(pk,'hex')
-        };
+    let opts = { initiator:true };
+    if (this.env === 'wrtc') {
+      opts.wrtc = this.wrtc;
     }
+    let peer = new Peer( opts );
 
-    async recv_bootstrap (pk, data) {
-        if (this.peers[pk] == undefined) {
-            let opts = {};
-            if (this.env == 'wrtc') {
-                opts.wrtc = this.wrtc;
-            }
+    // receive offer connection info.
+    peer.on('signal', async data => {
+      this.handlers.send_bootstrap(pk ,data);
+    });
 
-            let peer = new Peer( opts );
-            this.peers[pk] = {
-                peer: peer,
-                connected: false,
-                publicKey: Buffer.from(pk, 'hex')
-            }
+    peer.on('connect', () => {
+      this.peers[pk].connected = true;
+      console.log(pk, 'is connected')
+      func();
+    });
 
-            peer.on('connect', () => {
-                this.peers[pk].connected = true;
-                console.log(pk, 'is connected')
-            });
+    peer.on('close', () => {
+      // delete?
+      console.log(pk, 'is closed')
+      if (this.peers[pk]) {
+        this.peers[pk].connected = false;
+        this.peers[pk].peer.destroy();
+        delete this.peers[pk];
+      }
+    })
 
-            peer.on('close', () => {
-                // delete?
-                console.log(pk, 'is closed')
-                if (this.peers[pk]) {
-                    this.peers[pk].connected = false;
-                    this.peers[pk].peer.destroy();
-                    delete this.peers[pk];
-                }
-            })
+    peer.on('error', (err) => {
+      // delete?
+      console.log(pk, 'is error')
+      delete this.peers[pk];
+      if (this.peers[pk]) {
+        this.peers[pk].connected = false;
+        this.peers[pk].peer.destroy();
+        delete this.peers[pk];
+      }
+    });
 
-            peer.on('signal', data => {
-                this.handlers.send_bootstrap(pk, data);
-            })
+    peer.on('data', data => {
+      // Need add crypto
+      this.handlers.recv_data(pk,data);
+    });
 
-            peer.on('data', data => {
-                // Need add crypto
-                this.handlers.recv_data(pk,data);
-            });
+    this.peers[pk] = {
+      peer: peer,
+      connected: false,
+      publicKey: Buffer.from(pk,'hex')
+    };
+  }
 
-            peer.on('error', (err) => {
-                // delete?
-                console.log(pk, 'is error')
-                if (this.peers[pk]) {
-                    this.peers[pk].connected = false;
-                    this.peers[pk].peer.destroy();
-                    delete this.peers[pk];
-                }
-            });
-        };
+  async recv_bootstrap (pk, data) {
+    if (this.peers[pk] === undefined) {
+      let opts = {};
+      if (this.env === 'wrtc') {
+        opts.wrtc = this.wrtc;
+      }
 
-        let peer = this.peers[pk];
+      let peer = new Peer( opts );
+      this.peers[pk] = {
+        peer: peer,
+        connected: false,
+        publicKey: Buffer.from(pk, 'hex')
+      }
 
-        peer.peer.signal(data);
-    }
+      peer.on('connect', () => {
+        this.peers[pk].connected = true;
+        console.log(pk, 'is connected')
+      });
 
-    bootstrap (servers) {
-        let promise = new Promise((resolve,reject) => {
-            this._boot_server(servers, () => {
-                resolve();
-            });
-        });
-        return promise;
-    }
-
-    send(pk, data) {
+      peer.on('close', () => {
+        // delete?
+        console.log(pk, 'is closed')
         if (this.peers[pk]) {
-            this.peers[pk].peer.send(data);
-            return true;
-        } else {
-            return false;
+          this.peers[pk].connected = false;
+          this.peers[pk].peer.destroy();
+          delete this.peers[pk];
         }
+      })
+
+      peer.on('signal', data => {
+        this.handlers.send_bootstrap(pk, data);
+      })
+
+      peer.on('data', data => {
+        // Need add crypto
+        this.handlers.recv_data(pk,data);
+      });
+
+      peer.on('error', (err) => {
+        // delete?
+        console.log(pk, 'is error')
+        if (this.peers[pk]) {
+          this.peers[pk].connected = false;
+          this.peers[pk].peer.destroy();
+          delete this.peers[pk];
+        }
+      });
+    };
+
+    let peer = this.peers[pk];
+
+    peer.peer.signal(data);
+  }
+
+  bootstrap (servers) {
+    let promise = new Promise((resolve,reject) => {
+      this._boot_server(servers, () => {
+        resolve();
+      });
+    });
+    return promise;
+  }
+
+  send(pk, data) {
+    if (this.peers[pk]) {
+      this.peers[pk].peer.send(data);
+      return true;
+    } else {
+      return false;
     }
+  }
 }
 
 module.exports = AlptP2P
-
--- src/utils/file.js
+++ src/utils/file.js
@@ -1,6 +1,7 @@
 import P2P from './connection.js'
 
-    /*
+/* 
+
 let state = {}
 
 state.promises = {};
@@ -8,84 +9,84 @@ state.promises = {};
 state.promises.recv_data = {};
 
 state.promises.recv_data.promise = new Promise( ( resolve, reject ) => {
-    state.promises.recv_data.resolve = resolve;
-    state.promises.recv_data.reject = reject;
+  state.promises.recv_data.resolve = resolve;
+  state.promises.recv_data.reject = reject;
 } );
 
 state.promises.on_open.promise = new Promise( ( resolve, reject ) => {
-    state.promises.on_open.resolve = resolve;
-    state.promises.on_open.reject = reject;
+  state.promises.on_open.resolve = resolve;
+  state.promises.on_open.reject = reject;
 } );
 
 state.ws = new WebSocket('ws://localhost:3000/bootstrap');
 
 state.ws.onopen(() => {
-    state.p2p = new P2P({
-        handlers: {
-            send_bootstrap(pk, data) {
-                let s = {
-                    from: state.p2p.get_hex_key().publicKey,
-                    to: pk,
-                    data:data,
-                }
-                state.ws.send(JSON.stringify(s));
-            },
-            recv_data(pk, data) {
-                state.promises.recv_data.resolve({
-                    pk: pk,
-                    data: data,
-                })
-            }
-        }
-    });
-    console.log(`public key is ${state.p2p.get_hex_key().publicKey}`);
-    state.promises.on_open.resolve();
+  state.p2p = new P2P({
+    handlers: {
+      send_bootstrap(pk, data) {
+	let s = {
+	  from: state.p2p.get_hex_key().publicKey,
+	  to: pk,
+	  data:data,
+	}
+	state.ws.send(JSON.stringify(s));
+      },
+      recv_data(pk, data) {
+	state.promises.recv_data.resolve({
+	  pk: pk,
+	  data: data,
+	})
+      }
+    }
+  });
+  console.log(`public key is ${state.p2p.get_hex_key().publicKey}`);
+  state.promises.on_open.resolve();
 });
 
 state.ws.onmessage = (data) => {
-    let m = JSON.parse(data);
-    if (m.op == 'signal') {
-        console.log(`recv signal from ${m.from}`);
-        state.p2p.recv_bootstrap(m.from, m.data );
-    }
+  let m = JSON.parse(data);
+  if (m.op == 'signal') {
+    console.log(`recv signal from ${m.from}`);
+    state.p2p.recv_bootstrap(m.from, m.data );
+  }
 }
 
 */
 
 export default class FileTransfer {
-    constructor( prefix ) {
-        this.prefix = prefix;
+  constructor( prefix ) {
+    this.prefix = prefix;
 
-        this.ws = new WebSocket('ws://127.0.0.1:3000/bootstrap');
+    this.ws = new WebSocket('ws://127.0.0.1:3000/bootstrap');
 
-        this.p2p = new P2P( {
-            handlers: {
-                recv_bootstrap: (pk, data) => {
-                    this.ws.send( {
-                        from: this.p2p.get_hex_key().publicKey,
-                        to: pk,
-                        data: data,
-                        op: 'signal',
-                    } )
-                },
-                recv_data: (pk, data) => {
-                    console.log(`receive data from ${pk}`);
-                }
-            }
-        });
-
-        this.ws.onmessage = (data) => {
-            let m = JSON.parse(data);
-            if (m.op == 'signal') {
-                console.log(`recv signal from ${m.from}`);
-                this.p2p.recv_bootstrap(m.from, m.data );
-            }
+    this.p2p = new P2P( {
+      handlers: {
+        recv_bootstrap: (pk, data) => {
+          this.ws.send( {
+            from: this.p2p.get_hex_key().publicKey,
+            to: pk,
+            data: data,
+            op: 'signal',
+          } )
+        },
+        recv_data: (pk, data) => {
+          console.log(`receive data from ${pk}`);
         }
-    }
+      }
+    });
 
-    getUrl(files) {
-        this.files = files;
-        return this.prefix + '/' + this.p2p.get_hex_key().publicKey;
+    this.ws.onmessage = (data) => {
+      let m = JSON.parse(data);
+      if (m.op === 'signal') {
+        console.log(`recv signal from ${m.from}`);
+        this.p2p.recv_bootstrap(m.from, m.data );
+      }
     }
+  }
+
+  getUrl(files) {
+    this.files = files;
+    return this.prefix + '/' + this.p2p.get_hex_key().publicKey;
+  }
 }
 
